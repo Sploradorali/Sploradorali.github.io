f5e9ad2fbe5dbda4d886a3d8b62f0862bfbc505b
Dorali <chrislastra92@yahoo.com>
2019-01-24 00:54:15 -0800

    Added splash portal page; Unified pages; Fixed formatting and other aesthetic elements

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
-
 /*
-Nav Bar sticky function
+DOM Elements
 */
-window.onscroll = function(){ stickOnScroll() };
-
 var navbar = document.getElementById("navbar");
 var stickThreshold = navbar.offsetTop;
+var enterPortal = document.getElementById("portal-enter");
+var header = document.getElementById("expanding-header");
 
+window.onload = function() { header.style.height =  window.innerHeight + "px"; };
+
+window.onscroll = function(){ stickOnScroll() };
 function stickOnScroll() {
 	if (window.pageYOffset > stickThreshold) {
 		navbar.classList.add("sticky");
 	} else {
 		navbar.classList.remove("sticky");
 	}
+}
+
+enterPortal.onclick = function() { if(header.classList.contains("expanded")) collapseBanner() };
+function collapseBanner() {
+	enterPortal.style.visibility = "hidden";
+	header.style.height = header.scrollHeight + "px";
+	
+	document.documentElement.classList.remove("scroll-disabled");
+	document.body.classList.remove("scroll-disabled");
+	
+	document.getElementById("home-icon").style.visibility = "visible";
+	
+	header.classList.add("collapsed");
+	header.classList.remove("expanded");
+	
+	window.setTimeout(function() {
+		header.style.height = "220px";
+	}, 1);
+	
 }
