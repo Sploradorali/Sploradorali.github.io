197c0baea8200eaa52c01ab4b1c8c5d32fd77524
Chris <chrislastra92@yahoo.com>
2019-02-10 19:32:28 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var enterPortal = document.getElementById("portal-enter");
 var header = document.getElementById("expanding-header");
 
 window.onbeforeunload = function() { window.scrollTo(0,0); };
-window.onload = function() { header.style.height =  window.innerHeight + "px"; };
+//window.onload = function() { header.style.height =  window.innerHeight + "px"; };
 
 window.onscroll = function(){ stickOnScroll() };
 function stickOnScroll() {
@@ -18,7 +18,9 @@ function stickOnScroll() {
 	}
 }
 
+
 enterPortal.onclick = function() { if(header.classList.contains("expanded")) collapseBanner() };
+/*
 function collapseBanner() {
 	enterPortal.style.visibility = "hidden";
 	header.style.height = header.scrollHeight + "px";
@@ -36,3 +38,4 @@ function collapseBanner() {
 	}, 1);
 	
 }
+*/
